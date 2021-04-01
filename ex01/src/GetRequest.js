8f2af2a445de6d9210c15a38a4a2d101c014a342
DeniEdit <41287220+DeniEdit@users.noreply.github.com>
2021-04-01 17:22:40 +0200

    Day25

--- ex01/src/GetRequest.js
+++ ex01/src/GetRequest.js
@@ -3,19 +3,19 @@ import React, { Component } from 'react';
 class GetRequest extends React.Component {
     constructor(props) {
         super(props);
-        this.state = { totalReactPackage: null };
+        this.state = { totalReactPackages: null };
     }
     componentDidMount() {
         fetch('https://api.npms.io/v2/search?q=react')
         .then(response => response.json())
-        .then(data => this.setState({ totalReactPackage: data.total }));
+        .then(data => this.setState({ totalReactPackages: data.total }));
     }
     render() {
-        const { totalReactPackage } = this.state;
+        const { totalReactPackages } = this.state;
         return (
             <div className="card text-center m-3">
                 <h5 className="card-header">Simple GET Reguest</h5>
-                <div className="card-body">Total packages: {totalReactPackage}</div>
+                <div className="card-body">Total packages: {totalReactPackages}</div>
             </div>
         );
     }
