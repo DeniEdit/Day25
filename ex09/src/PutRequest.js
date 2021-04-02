fa40ed955de60f29a2488bde7121bd3f0f057b93
DeniEdit <41287220+DeniEdit@users.noreply.github.com>
2021-04-02 15:49:17 +0200

    Exercise 09

--- ex09/src/PutRequest.js
+++ ex09/src/PutRequest.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class PutRequest extends Component {
+class PutRequest extends React.Component {
     constructor(props) {
         super(props);
         this.state = { postId: null };
