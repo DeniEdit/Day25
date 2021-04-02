0e95a1f2786143b95cd17cf77c877dec84b968d4
DeniEdit <41287220+DeniEdit@users.noreply.github.com>
2021-04-02 15:50:10 +0200

    Exercise 10

--- ex10/src/DeleteRequest.js
+++ ex10/src/DeleteRequest.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class DeleteRequest extends Component {
+class DeleteRequest extends React.Component {
     constructor(props) {
         super(props);
         this.state = { status: null };
