7cbb7d43a24c476ae82f5de5c54dab3b3f793def
DeniEdit <41287220+DeniEdit@users.noreply.github.com>
2021-04-02 15:48:19 +0200

    Exercise 07

--- ex07/src/PostRequestErrorHandling.js
+++ ex07/src/PostRequestErrorHandling.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class PostRequestErrorHandling extends Component {
+class PostRequestErrorHandling extends React.Component {
     constructor(props) {
         super(props);
         this.state = { articleId: null, errorMessage: null };
